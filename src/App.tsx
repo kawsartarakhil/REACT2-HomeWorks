9d3ad90b062a045dcb59ecf01d631cc69cd41b28
kawsartarakhil <kawsartarakhil9@gmail.com>
2026-03-12 14:46:28 +0500

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React, { useState, useContext } from "react";
 import { useFormik } from "formik";
 import * as Yup from "yup";
 import { useTodo } from "./store/todo";
-import { ThemeContext} from "./mode/themeContext";
+import { ThemeContext } from "./mode/themeContext";
 
 const addSchema = Yup.object().shape({
   name: Yup.string()
@@ -20,17 +20,16 @@ const addSchema = Yup.object().shape({
     .required("Age is required"),
   status: Yup.boolean(),
 });
- 
 
 export default function App() {
-   const { data, addUser, editUser, deleteUser, changeStatus } = useTodo();
+  const { data, addUser, editUser, deleteUser, changeStatus } = useTodo();
   const { theme, setTheme } = useContext(ThemeContext);
 
   const [idx, setIdx] = useState(null);
   const [openAdd, setOpenAdd] = useState(false);
   const [openEdit, setOpenEdit] = useState(false);
- const [search, setSearch] = useState(""); 
-const [filterStatus, setFilterStatus] = useState("all"); 
+  const [search, setSearch] = useState("");
+  const [filterStatus, setFilterStatus] = useState("all");
   const formik = useFormik({
     initialValues: {
       name: "",
@@ -72,17 +71,19 @@ const [filterStatus, setFilterStatus] = useState("all");
     setIdx(user.id);
     setOpenEdit(true);
   };
-  const filters =data.filter((u)=>{
-    const searchUsers = u.name.toLowerCase().includes(search.toLocaleLowerCase())||
-    u.surname.toLowerCase().includes(search.toLocaleLowerCase());
-    const statusFilter= filterStatus=='all'||(filterStatus==='active'&&u.status) || (filterStatus==='inactive'&&!u.status);
-    return searchUsers&&statusFilter;
-  })
-  
+  const filters = data.filter((u) => {
+    const searchUsers =
+      u.name.toLowerCase().includes(search.toLocaleLowerCase()) ||
+      u.surname.toLowerCase().includes(search.toLocaleLowerCase());
+    const statusFilter =
+      filterStatus == "all" ||
+      (filterStatus === "active" && u.status) ||
+      (filterStatus === "inactive" && !u.status);
+    return searchUsers && statusFilter;
+  });
 
   return (
-   
-       <div
+    <div
       className={
         theme === "light"
           ? "bg-white text-black min-h-screen flex flex-col items-center"
@@ -91,8 +92,18 @@ const [filterStatus, setFilterStatus] = useState("all");
     >
       <h1 className="text-4xl font-bold m-10">User List</h1>
       <div className="flex gap-5 mb-5">
-        <input type="search" className="border h-10 " placeholder="Search" value={search} onChange={(e)=>setSearch(e.target.value)}/>
-        <select className={`border  p-2 shadow-md ${theme=="dark"? "bg-black text-white":"bg-white text-black"}`} value={filterStatus} onChange={(e)=>setFilterStatus(e.target.value)}>
+        <input
+          type="search"
+          className="border h-10 "
+          placeholder="Search"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <select
+          className={`border  p-2 shadow-md ${theme == "dark" ? "bg-black text-white" : "bg-white text-black"}`}
+          value={filterStatus}
+          onChange={(e) => setFilterStatus(e.target.value)}
+        >
           <option value="all">all</option>
           <option value="active">active</option>
           <option value="inactive">inactive</option>
@@ -109,70 +120,72 @@ const [filterStatus, setFilterStatus] = useState("all");
       </div>
 
       {(openAdd || openEdit) && (
-  <dialog open>
-    <form
-      onSubmit={formik.handleSubmit}
-      className="flex flex-col gap-3 p-5"
-    >
-      <input
-        name="name"
-        placeholder="name"
-        value={formik.values.name}
-        onChange={formik.handleChange}
-      />
-      {formik.errors.name && formik.touched.name && (
-        <span className="text-red-600 text-sm">{formik.errors.name}</span>
+        <dialog open>
+          <form
+            onSubmit={formik.handleSubmit}
+            className="flex flex-col gap-3 p-5"
+          >
+            <input
+              name="name"
+              placeholder="name"
+              value={formik.values.name}
+              onChange={formik.handleChange}
+            />
+            {formik.errors.name && formik.touched.name && (
+              <span>{formik.errors.name}</span>
+            )}
+
+            <input
+              name="surname"
+              placeholder="surname"
+              value={formik.values.surname}
+              onChange={formik.handleChange}
+            />
+            {formik.errors.surname && formik.touched.surname && (
+              <span className="text-red-600 text-sm">
+                {formik.errors.surname}
+              </span>
+            )}
+
+            <input
+              name="age"
+              type="number"
+              value={formik.values.age}
+              onChange={(e) =>
+                formik.setFieldValue("age", Number(e.target.value))
+              }
+            />
+            {formik.errors.age && formik.touched.age && (
+              <span className="text-red-600 text-sm">{formik.errors.age}</span>
+            )}
+
+            <label>
+              <input
+                type="checkbox"
+                name="status"
+                checked={formik.values.status}
+                onChange={(e) =>
+                  formik.setFieldValue("status", e.target.checked)
+                }
+              />
+              Status
+            </label>
+
+            <button type="submit">{idx !== null ? "Update" : "Save"}</button>
+            <button
+              type="button"
+              onClick={() => {
+                setOpenAdd(false);
+                setOpenEdit(false);
+                formik.resetForm();
+                setIdx(null);
+              }}
+            >
+              Close
+            </button>
+          </form>
+        </dialog>
       )}
-
-      <input
-        name="surname"
-        placeholder="surname"
-        value={formik.values.surname}
-        onChange={formik.handleChange}
-      />
-      {formik.errors.surname && formik.touched.surname && (
-        <span className="text-red-600 text-sm">{formik.errors.surname}</span>
-      )}
-
-      <input
-        name="age"
-        type="number"
-        value={formik.values.age}
-        onChange={(e) =>
-          formik.setFieldValue("age", Number(e.target.value))
-        }
-      />
-      {formik.errors.age && formik.touched.age && (
-        <span className="text-red-600 text-sm">{formik.errors.age}</span>
-      )}
-
-      <label>
-        <input
-          type="checkbox"
-          name="status"
-          checked={formik.values.status}
-          onChange={(e) =>
-            formik.setFieldValue("status", e.target.checked)
-          }
-        />{" "}
-        Status
-      </label>
-
-      <button type="submit">{idx !== null ? "Update" : "Save"}</button>
-      <button
-        type="button"
-        onClick={() => {
-          setOpenAdd(false);
-          setOpenEdit(false);
-          formik.resetForm();
-          setIdx(null);
-        }}
-      >
-        Close
-      </button>
-    </form>
-  </dialog>
-)}
 
       <table className="border mt-10 w-[600px] text-center">
         <thead>
@@ -217,6 +230,5 @@ const [filterStatus, setFilterStatus] = useState("all");
         </tbody>
       </table>
     </div>
-    
   );
 }
